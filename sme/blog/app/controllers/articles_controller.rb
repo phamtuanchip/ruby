742cb96b1872ab4d13e9bb1ac1267d0f6ff4454d
pham tuan <phamtuanchip@gmail.com>
2015-04-07 17:57:36 +0700

    add rails example

---
+++ sme/blog/app/controllers/articles_controller.rb
@@ -0,0 +1,48 @@
+class ArticlesController < ApplicationController
+	def index
+     @articles = Article.all
+    end
+	def show
+		@article = Article.find(params[:id])
+	end
+	
+	def new
+		@article = Article.new
+    end
+	
+	 def create
+		 render plain: params[:article].inspect
+		 @article = Article.new(article_params)
+ 
+		if @article.save
+			 return @article
+			 
+		else
+		   render 'new'
+		 
+		end
+		
+     end 
+	 
+	 def edit
+		@article = Article.find(params[:id])
+	 end
+	 
+	  
+	def update
+	  @article = Article.find(params[:id])
+	 
+	  if @article.update(article_params)
+		 return @article) 
+		 
+	  else
+		render 'edit'  
+		 
+	  end
+	end
+	
+	private
+	  def article_params
+		params.require(:article).permit(:title, :text)
+	  end
+end
